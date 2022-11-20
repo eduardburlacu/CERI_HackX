8a534da043fcd5abc2b06bc198c93a52c309f428
Daniel Nicolae <cdn27@cam.ac.uk>
2022-11-20 12:32:01 +0000

    predicted other data

--- Covid_Cases_Predictor-main/script_model2.py
+++ Covid_Cases_Predictor-main/script_model2.py
@@ -2,10 +2,14 @@ from tensorflow.keras.models import load_model
 import pandas as pd
 import os
 import numpy as np
+import pickle
+
+model_filename = os.path.join(os.getcwd(), "Covid_Cases_Predictor-main", "model2")
+data_dirname = os.path.join(os.getcwd(), "csvs")
 
 def get_all_data():
     list_of_df= []
-    for (root, dirs, files) in os.walk(os.path.join(os.getcwd(), "county_data")):
+    for (root, dirs, files) in os.walk(data_dirname):
         for file in files:
             if file[-4:] == ".csv" and os.path.getsize(os.path.join(root, file)) > 100:  
                 df = pd.read_csv(os.path.join(root, file))
@@ -32,8 +36,7 @@ def get_current_trends():
             trends_dict[name][feature] = (round(slope, 4), round(data[feature].mean(), 2))
     return trends_dict
 
-def get_y_data(n, list_of_df):
-    past_values = 100
+def get_y_data(n, list_of_df, past_values=100):
     values=list(list_of_df[n][1]["True_Positive"])
     X = []
     y = []
@@ -47,7 +50,7 @@ def get_y_data(n, list_of_df):
 
 def get_predicted(steps):
     list_of_df = get_all_data()
-    model = load_model("model2")
+    model = load_model(model_filename)
     prediction_dict = {}
     for i in range(len(list_of_df)):
         X, y = get_y_data(i, list_of_df)
@@ -58,6 +61,28 @@ def get_predicted(steps):
             timeline = np.append(timeline, y_pred)
         prediction_dict[list_of_df[i][0]] = timeline[-steps:]
     return prediction_dict
+
+def get_other_predicted():
+
+    list_of_df = get_all_data()
+    prediction_dict = {}
+
+    # initialize dict of dicts
+    for i in range(len(list_of_df)):
+        prediction_dict[list_of_df[i][0]] = {}
+
+    for feat in ["Cases", "Deaths", "Tests"]:
+        with open(os.path.join(os.getcwd(), "Covid_Cases_Predictor-main", "model_"+feat),'rb') as f:
+            model = pickle.load(f)
+        for i in range(len(list_of_df)):
+            X, y = get_y_data(i, list_of_df, past_values=50)
+            y_future = model.predict(X[-1:])
+            y_future = y_future[0]
+            for j, val in enumerate(y_future):
+                if val < 0: y_future[j] = 0
+            prediction_dict[list_of_df[i][0]][feat] = np.round(y_future)
+    return prediction_dict
+    
 
 def test_get_y_data():
     list_of_df = get_all_data()
